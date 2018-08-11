d32cb34296d49f14eaf702e99e91ab5f42df8f5a
Chen Yu Pao <windperson@gmail.com>
2018-08-11 17:31:14 +0800

    A complete CRUD example with associated (POST)/(GET)/(PATCH or PUT)/(Delete) REST API

--- src/ServerAPI/Controllers/BooksController.cs
+++ src/ServerAPI/Controllers/BooksController.cs
@@ -1,6 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 using Microsoft.AspNet.OData;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ServerAPI.Model;
 
 namespace ServerAPI.Controllers
@@ -12,7 +16,11 @@ namespace ServerAPI.Controllers
         public BooksController(BookStoreContext context)
         {
             _db = context;
-            if (context.Books.Any()){ return;}
+            if (context.Books.Any())
+            {
+                return;
+            }
+
             foreach (var book in DataSource.GetBooks())
             {
                 context.Books.Add(book);
@@ -22,29 +30,104 @@ namespace ServerAPI.Controllers
             context.SaveChanges();
         }
 
+        #region OData CRUD
+
         [EnableQuery]
-        public IActionResult Get(int? key)
+        public async Task<IActionResult> Get([FromODataUri] int? key)
         {
             if (key.HasValue)
             {
-                return Ok(_db.Books.FirstOrDefault(c => c.Id == key));
+                return Ok(await _db.Books.FindAsync(key));
             }
+
             return Ok(_db.Books);
         }
 
-//        [EnableQuery]
-//        public IActionResult Get(int key)
-//        {
-//            return Ok(_db.Books.FirstOrDefault(c => c.Id == key));
-//        }
-
         [EnableQuery]
-        public IActionResult Post([FromBody] Book book)
+        public async Task<IActionResult> Post([FromBody] Book book)
         {
-            _db.Books.Add(book);
-            _db.SaveChanges();
+            await _db.Books.AddAsync(book);
+            await _db.SaveChangesAsync();
             return Created(book);
         }
 
+        [EnableQuery]
+        public async Task<IActionResult> Patch([FromODataUri] int key, [FromBody] Delta<Book> book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var entity = await _db.Books.FindAsync(key);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+
+            book.Patch(entity);
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (await _db.Books.AnyAsync(x => x.Id == key))
+                {
+                    throw;
+                }
+
+                return NotFound();
+            }
+
+            return Updated(entity);
+        }
+
+        [EnableQuery]
+        public async Task<IActionResult> Put([FromODataUri] int key, [FromBody] Book updatedBook)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (key != updatedBook.Id)
+            {
+                return BadRequest();
+            }
+
+            _db.Entry(updatedBook).State = EntityState.Modified;
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException )
+            {
+                if (await _db.Books.AnyAsync(x => x.Id == key))
+                {
+                    throw;
+                }
+
+                return NotFound();
+            }
+
+            return Updated(updatedBook);
+        }
+
+        [EnableQuery]
+        public async Task<IActionResult> Delete([FromODataUri] int key)
+        {
+            var target = await _db.FindAsync<Book>(key);
+            if (target == null)
+            {
+                return NotFound();
+            }
+
+            _db.Books.Remove(target);
+            await _db.SaveChangesAsync();
+
+            return StatusCode((int) HttpStatusCode.NoContent);
+        }
+        #endregion
     }
 }
